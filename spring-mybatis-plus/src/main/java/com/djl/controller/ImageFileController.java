ac2d81340aa40f5dd9e99626b0b3b34a5a0b521f
90411 <90411@MSI>
2018-05-05 14:19:14 +0800

    11

--- spring-mybatis-plus/src/main/java/com/djl/controller/ImageFileController.java
+++ spring-mybatis-plus/src/main/java/com/djl/controller/ImageFileController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class ImageFileController {
 	@Value("${image_filepath}")
-	private String filepath;
+	private String filepath; 
 
 	private static final Logger logger =LoggerFactory.getLogger(ImageFileController.class);
 	/**
